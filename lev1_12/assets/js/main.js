4a3368198848943146050263547a51ac9588a987
Hanni <ngynngchn@gmail.com>
2023-01-03 14:19:15 +0100

    Initial commmit

---
+++ lev1_12/assets/js/main.js
@@ -0,0 +1,25 @@
+// Punkt vor Strich Rechnung
+// let score = 5 + 5 * 10;
+// console.log("Ergebnis :" + score);
+
+//Klammer
+// let score = (5 + 5) * 10;
+// console.log("Ergebnis:" + score);
+
+// Werte auf Variablen addieren
+// let score = 0;
+// score = score + 10;
+// score += 10;
+// console.log("Ergebnis:" + score);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+console.log(zahl);
+
+zahl++;
+console.log("increment: " + zahl);
+
+zahl--;
+console.log("decrement: " + zahl);
